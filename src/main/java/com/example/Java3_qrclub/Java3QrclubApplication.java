93ff5a18ab064d1f4ab5fff8998bf6530bc58eb3
Azaliya <ziganshina.azaliya1@gmail.com>
2026-01-19 22:52:40 +0300

    Test

---
+++ src/main/java/com/example/Java3_qrclub/Java3QrclubApplication.java
@@ -0,0 +1,13 @@
+package com.example.Java3_qrclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java3QrclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java3QrclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Java3_qrclub/Java3QrclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java3_qrclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java3QrclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
